a76d93f52e45d438f48b36c73f8758675c373eb8
Serdar Bayramov <serdartmbn@gmail.com>
2023-09-29 22:56:45 +0100

    accept both changes

--- git_file.py
+++ git_file.py
@@ -1,8 +1,5 @@
 print('my second commit')
 print('1st change in feature1')
-<<<<<<< HEAD
 print('feature2 1st commit')
 print('feature2 2nd commit')
-=======
 print('2nd change from feature2')
->>>>>>> feature2
